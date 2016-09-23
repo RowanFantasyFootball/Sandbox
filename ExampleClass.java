4058982c9ceeecce8a939d08fe4b6424f259666e
Dlackey75 <dlackey75@yahoo.com>
2016-09-23 09:31:12 -0400

    Updated sampleMethod

--- ExampleClass.java
+++ ExampleClass.java
@@ -25,9 +25,8 @@ public class ExampleClass
      * @param  y   a sample parameter for a method
      * @return     the sum of x and y 
      */
-    public int sampleMethod(int y)
+    public int sampleMethod()
     {
-        // put your code here
-        return x + y;
+        
     }
 }
